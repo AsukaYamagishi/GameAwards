81fc10b9621aa7b9174c8f91211c34235a6c43ba
125moti <k020g1385@g.neec.ac.jp>
2022-02-25 14:05:10 +0900

    no message

--- ModelManager.cpp
+++ ModelManager.cpp
@@ -11,11 +11,6 @@ ModelManager *ModelManager::GetIns()
 void ModelManager::Initialize()
 {
 	LoadModel(Player, "player");
-<<<<<<< HEAD
-=======
-	LoadModel(PlayerBullet, "bullet");
-	LoadModel(Enemy, "enemy");
->>>>>>> origin/エンジン
 	//LoadModel(TestDunut, "donut");
 }
 
